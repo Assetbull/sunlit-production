987dea123a408175ef00ccc4309ffaab62adf533
Sunlit Energy <sunlit@sunlitenergy.com>
2026-08-08 15:32:07 +0100

    chore: add vercel.json & optimize next.config.ts for seamless Vercel deployment

--- sunlit-app/next.config.ts
+++ sunlit-app/next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
